87a534ec011e3e1ec59d06df83725a28d2d3f335
SecretKeeper <shervin.ourfamily@gmail.com>
2022-06-03 18:33:24 +0430

    init

---
+++ src/components/icons/image.tsx
@@ -0,0 +1,29 @@
+import { FunctionComponent, h, JSX } from 'preact';
+
+type Props = {
+  width: number;
+  height: number;
+};
+
+const ImageSVG: FunctionComponent<Props> = ({ width, height }): JSX.Element => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      width={width}
+      height={height}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <rect x="3" y="3" width="18" height="18" rx="2" ry="2" />
+      <circle cx="8.5" cy="8.5" r="1.5" />
+      <polyline points="21 15 16 10 5 21" />
+    </svg>
+  );
+};
+
+export default ImageSVG;
