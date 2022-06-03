f24101243de82238004842b003bf8a45081a10d0
SecretKeeper <shervin.ourfamily@gmail.com>
2022-06-03 18:43:28 +0430

    init

---
+++ src/components/icons/index.ts
@@ -0,0 +1,25 @@
+import BoldSVG from '../icons/bold';
+import UnderlineSVG from '../icons/underline';
+import ItalicSVG from '../icons/italic';
+import StrikethroughSVG from '../icons/strikethrough';
+import QuoteSVG from '../icons/quote';
+import NumberedListSVG from '../icons/numberedList';
+import BulletedListSVG from '../icons/bulletedList';
+import CodeSVG from '../icons/code';
+import SeparatorSVG from '../icons/separator';
+import ImageSVG from '../icons/image';
+import LinkSVG from '../icons/link';
+
+export {
+  BoldSVG,
+  UnderlineSVG,
+  ItalicSVG,
+  StrikethroughSVG,
+  QuoteSVG,
+  NumberedListSVG,
+  BulletedListSVG,
+  CodeSVG,
+  SeparatorSVG,
+  ImageSVG,
+  LinkSVG,
+};
