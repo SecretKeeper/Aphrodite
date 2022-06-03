578bad16ba547e5f9e13a604d94964942bc7cece
SecretKeeper <shervin.ourfamily@gmail.com>
2022-06-03 18:33:33 +0430

    init

---
+++ src/components/icons/link.tsx
@@ -0,0 +1,30 @@
+import { FunctionComponent, h, JSX } from 'preact';
+
+type Props = {
+  width: number;
+  height: number;
+};
+
+const LinkSVG: FunctionComponent<Props> = ({ width, height }): JSX.Element => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      width={width}
+      height={height}
+      x="0px"
+      y="0px"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path d="M15 7h3a5 5 0 0 1 5 5 5 5 0 0 1-5 5h-3m-6 0H6a5 5 0 0 1-5-5 5 5 0 0 1 5-5h3" />
+      <line x1="8" y1="12" x2="16" y2="12" />
+    </svg>
+  );
+};
+
+export default LinkSVG;
