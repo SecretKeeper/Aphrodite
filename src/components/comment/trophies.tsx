8e653268666f9616b91c0429eb63a55c31298073
SecretKeeper <shervin.ourfamily@gmail.com>
2022-06-06 14:20:43 +0430

    init

---
+++ src/components/comment/trophies.tsx
@@ -0,0 +1,38 @@
+import { FunctionComponent, h } from 'preact';
+import { BeerSVG, CrownSVG, MedalSVG, TargetSVG, WineSVG } from '../icons';
+
+export interface Trophy {
+  type: number;
+  count: number;
+}
+
+type Props = {
+  trophies: Trophy[];
+};
+
+const Trophies: FunctionComponent<Props> = (props): JSX.Element => {
+  const trophiesDic: { [key: number]: JSX.Element } = {
+    1: <BeerSVG width={28} height={28} />,
+    2: <WineSVG width={28} height={28} />,
+    3: <MedalSVG width={28} height={28} />,
+    4: <TargetSVG width={28} height={28} />,
+    5: <CrownSVG width={28} height={28} />,
+  };
+
+  return (
+    <div className="flex ml-auto">
+      {props.trophies.map((trophy: Trophy, index: number) => {
+        return (
+          <span className="relative mr-15px" key={index}>
+            {trophiesDic[trophy.type]}
+            <span className="absolute flex justify-center items-center bottom-18px left-18px w-15px h-15px font-semibold text-center text-xs leading-3 bg-hex-323232 text-white rounded-full">
+              {trophy.count}
+            </span>
+          </span>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Trophies;
