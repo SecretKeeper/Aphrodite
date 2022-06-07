094ae949b9662974cfd97a463e07b69ef8db9788
SecretKeeper <shervin.ourfamily@gmail.com>
2022-06-07 11:48:21 +0430

    init

---
+++ src/components/icons/userX.tsx
@@ -0,0 +1,30 @@
+import { FunctionComponent, h, JSX } from 'preact';
+
+type Props = {
+  width: number;
+  height: number;
+};
+
+const UserXSVG: FunctionComponent<Props> = ({ width, height }): JSX.Element => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      width={width}
+      height={height}
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      viewBox="0 0 25 25"
+    >
+      <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2" />
+      <circle cx="8.5" cy="7" r="4" />
+      <line x1="18" y1="8" x2="23" y2="13" />
+      <line x1="23" y1="8" x2="18" y2="13" />
+    </svg>
+  );
+};
+
+export default UserXSVG;
