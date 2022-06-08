868343ffd91cebbed08d4d73c9055ca20d82d3f9
SecretKeeper <shervin.ourfamily@gmail.com>
2022-06-08 11:28:33 +0430

    code refactoring

--- src/components/comment/meta/report.tsx
+++ src/components/comment/meta/report.tsx
@@ -1,21 +1,9 @@
 import { FunctionComponent, h } from 'preact';
+import { FlagSVG } from '../../icons';
 
 const FavoriteBadge: FunctionComponent = (): JSX.Element => (
-  <span className="align-middle pl-8px">
-    <svg
-      className="transition-all duration-150"
-      fill="none"
-      stroke="#d75a4a"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      width={17}
-      height={17}
-      viewBox="0, -2, 25, 25"
-    >
-      <path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1z" />
-      <line x1="4" y1="22" x2="4" y2="15" />
-    </svg>
+  <span className="align-middle pl-2">
+    <FlagSVG width={17} height={17} />
   </span>
 );
 
